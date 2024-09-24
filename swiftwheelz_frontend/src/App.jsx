c44e33035dacdec6fa5693919c84e9af3dafb7af
MaleselaModiba <222140275@mycput.ac.za>
2024-09-24 16:53:19 +0200

    Updated Routes

--- swiftwheelz_frontend/src/App.jsx
+++ swiftwheelz_frontend/src/App.jsx
@@ -88,7 +88,7 @@ function App() {
                         <Route path="/customer/*" element={<CustomerLayout />}>
                             <Route path="profile" element={<CustomerProfile />} />
                             <Route path="pending-payments" element={<PendingPayments />} />
-                            <Route path="customer/rentals" element={<RentalsList/>}/>
+                            <Route path="rentals-list" element={<RentalsList/>}/>
                         </Route>
 
                         <Route path="/get-quote/:truckId" element={<GetQuote />} />
